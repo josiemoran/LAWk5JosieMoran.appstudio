ed3f694d7875cdb2e9879476da694fa1b2b47c8d
josiemoran <79425452+josiemoran@users.noreply.github.com>
2021-03-05 21:20:29 -0600

    Calc Code

--- forms/calc/calc.js
+++ forms/calc/calc.js
@@ -1,3 +1,10 @@
-/*function Calculate(num1, num2)
-let summedNumbers
-*/
+function calculate(num1, num2) {
+    let sum = (num1 + num2)
+    return sum
+}
+
+let numb1 = Number(prompt("Enter Number:"))
+let numb2 = Number(prompt("Enter Another Number:"))
+
+let summedNumbers = calculate (numb1, numb2)
+console.log(`The sum of ${numb1} and ${numb2} is ${summedNumbers}.`)
